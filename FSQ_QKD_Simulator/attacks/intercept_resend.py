8ea66fcf4a6468a72bf6e3e1d7be144682e93115
Samuel Weinzaepfel <weinzaepfel.samuel@gmail.com>
2026-09-19 21:35:43 +0200

    Add files via upload

---
+++ FSQ_QKD_Simulator/attacks/intercept_resend.py
@@ -0,0 +1,39 @@
+# Import the necessary package(s)
+import numpy as np
+import qiskit as qk
+
+
+# Create Eve's choices generator
+def generate_eve_choices(number_of_signals, interception_probability):
+    # Create the decision for Eve to intercept
+    eve_intercepts = []
+
+    for i in range(number_of_signals):
+        random_decimal = np.random.random()
+        decision = random_decimal < interception_probability
+        eve_intercepts.append(decision)
+
+    # Create Eve's bases
+    eve_bases = []
+
+    for i in range(number_of_signals):
+        random_bit = np.random.randint(0,2)
+        eve_bases.append(random_bit)
+    
+    return (eve_intercepts, eve_bases)
+
+
+# Create the interception and resend to Bob function
+def intercept_and_resend(qc, eve_intercepts, eve_bases):
+
+    for i in range(len(eve_intercepts)):
+        if eve_intercepts[i]:
+            if eve_bases[i] == 0:
+                qc.measure(i,i)
+
+            else:
+                qc.h(i)
+                qc.measure(i,i)
+                qc.h(i)
+
+    return(qc)
