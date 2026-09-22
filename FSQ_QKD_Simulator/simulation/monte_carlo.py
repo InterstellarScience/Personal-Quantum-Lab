176e3b59b53af14ca5cbc8b4a85f1d003a89a94b
Samuel Weinzaepfel <weinzaepfel.samuel@gmail.com>
2026-09-22 11:26:58 +0200

    Add misalignment and background count flags to transmissions

--- FSQ_QKD_Simulator/simulation/monte_carlo.py
+++ FSQ_QKD_Simulator/simulation/monte_carlo.py
@@ -4,14 +4,15 @@ from attacks.intercept_resend import(intercept_single_qubit)
 import numpy as np
 
 
+# Simulator for the real qiskit circuit (slow)
 # Create the simulation for every single transmissible qubit
-def run_transmissions(alice_bits, alice_bases, bob_bases, eve_intercepts, eve_bases, detected_photon_numbers, dark_count_flags):
+def run_transmissions(alice_bits, alice_bases, bob_bases, eve_intercepts, eve_bases, detected_photon_numbers, dark_count_flags, misalignment_flags, background_count_flags):
     bob_bits = []
 
     for i in range(len(alice_bits)):
         # Checking if pulses arrived at Bob's station
         if detected_photon_numbers[i] == 0:
-            if dark_count_flags[i]:
+            if dark_count_flags[i] or background_count_flags[i]:
                 bob_bits.append(np.random.randint(0, 2))
             else:
                 bob_bits.append(None)
@@ -22,7 +23,15 @@ def run_transmissions(alice_bits, alice_bases, bob_bases, eve_intercepts, eve_ba
         qc = prepare_single_qubit(alice_bits[i], alice_bases[i])
         qc = intercept_single_qubit(qc, eve_intercepts[i], eve_bases[i])
         qc = measure_single_qubit_bob(qc, bob_bases[i])
-        bob_bits.append(simulate_single_qubit(qc))
+        
+        # Check for misalignment of encoding and measurement bases
+        bob_bit = simulate_single_qubit(qc)
+
+        if misalignment_flags[i]:
+            bob_bit = 1 - bob_bit 
+
+        # Append to list
+        bob_bits.append(bob_bit)
 
     return (bob_bits)
 
@@ -36,13 +45,13 @@ def measure_bit(prepared_bit, prepared_basis, measurement_basis):
         return (np.random.randint(0,2))
 
 # Create the fast simulation
-def run_transmissions_fast(alice_bits, alice_bases, bob_bases, eve_intercepts, eve_bases, detected_photon_numbers, dark_count_flags):
+def run_transmissions_fast(alice_bits, alice_bases, bob_bases, eve_intercepts, eve_bases, detected_photon_numbers, dark_count_flags, misalignment_flags, background_count_flags):
     bob_bits = []
 
     for i in range(len(alice_bits)):
         # Checking if pulses arrived at Bob's station
         if detected_photon_numbers[i] == 0:
-            if dark_count_flags[i]:
+            if dark_count_flags[i] or background_count_flags[i]:
                 bob_bits.append(np.random.randint(0, 2))
             else:
                 bob_bits.append(None)
@@ -53,9 +62,14 @@ def run_transmissions_fast(alice_bits, alice_bases, bob_bases, eve_intercepts, e
         if eve_intercepts[i]:
             eve_bit = measure_bit(alice_bits[i], alice_bases[i], eve_bases[i])
             bob_bit = measure_bit(eve_bit, eve_bases[i],  bob_bases[i])
-            bob_bits.append(bob_bit)
         else:
+            # Measure qubit
             bob_bit = measure_bit(alice_bits[i], alice_bases[i], bob_bases[i])
-            bob_bits.append(bob_bit)
+
+        # Check for misalignment of encoding and measurement bases
+        if misalignment_flags[i]:
+            bob_bit = 1 - bob_bit
+
+        bob_bits.append(bob_bit)
 
     return (bob_bits)
